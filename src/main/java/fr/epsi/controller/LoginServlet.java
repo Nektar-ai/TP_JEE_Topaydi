bcfd783edee5169672c0c05f3e4940e814dcdecc
j0hnd03 <j0hnd03@bl4d3>
2021-04-09 15:37:39 +0200

    start

--- src/main/java/fr/epsi/controller/LoginServlet.java
+++ src/main/java/fr/epsi/controller/LoginServlet.java
@@ -2,19 +2,33 @@ package fr.epsi.controller;
 
 import java.io.IOException;
 
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import fr.epsi.entity.User;
+import fr.epsi.service.UserService;
+
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 
+	@EJB
+	private UserService service;
 	
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException
     {
 		this.getServletContext().getRequestDispatcher("/WEB-INF/pages/Login.jsp").forward(req, resp);
     }
+    
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
+	{
+    	User u = new User();
+    	u.setNickname((String) req.getParameter("nickname"));
+    	u.setPassword((String) req.getParameter("password"));
+    	service.createDummyUser(u);
+	}
 }
--- src/main/java/fr/epsi/service/UserServiceImpl.java
+++ src/main/java/fr/epsi/service/UserServiceImpl.java
@@ -1,15 +1,28 @@
 package fr.epsi.service;
 
+import javax.annotation.Resource;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
 
 import fr.epsi.entity.User;
 import fr.epsi.repository.UserRepository;
 import fr.epsi.repository.UserRepositoryImpl;
 
 @Stateless
+@TransactionManagement(TransactionManagementType.BEAN)
 public class UserServiceImpl implements UserService {
 
+	@PersistenceContext(unitName = "topaydiPU")
+	EntityManager em;
+	
+	@Resource
+	UserTransaction utx;
+	
 	@EJB
 	UserRepository dao = new UserRepositoryImpl();
 	
